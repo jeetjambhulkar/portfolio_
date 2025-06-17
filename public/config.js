001964be2f4841b699a39fae79784e79d53076db
jeetjanbhulkar <jambhulkarjeet841@gmail.com>
2025-06-17 11:18:47 +0530

    minor update

---
+++ public/config.js
@@ -0,0 +1,4 @@
+const config = {
+    SUPABASE_URL: 'https://vmqvfjdavlxvaozakemt.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtcXZmamRhdmx4dmFvemFrZW10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMzY3MzYsImV4cCI6MjA2NTYxMjczNn0.m4xBqfHUtkyOxt1ymn2DW4L5pZMYbdkRENlDPvQsLRg'
+}; 
